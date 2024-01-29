98eef978dcd3ffcb9b45bc3950267cb64c13ae88
NikitaSoftware <nekitdar@hotmail.com>
2024-01-29 11:00:12 +0200

    add table overflow and set width for control panel

--- src/helpers/table-render.tsx
+++ src/helpers/table-render.tsx
@@ -39,4 +39,6 @@ export const getHeadings = (sort: SortOptions, order: OrderOption) => {
 
 export const getRows = (data: RenderData[] = []) => data.slice(0, 10).map((obj, i) => <tr key={i} className='total-table__row'>{getCells(obj)}</tr>);
 export const getTotalRows = (data: RenderData[] = []) => data.map((obj, i) => <tr key={i} className='total-table__row'>{getCells(obj)}</tr>);
-export const getCells = (obj: RenderData) => Object.values(obj).map((value: any, i) => <td key={i} className='total-table__item'>{value.toLocaleString()}</td>);
+export const getCells = (obj: RenderData) => Object.values(obj).map((value: any, i) => <td key={i} className='total-table__item'>
+  <div className="total-table__overflow">{value.toLocaleString()}</div>
+</td>);
